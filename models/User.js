982c4cb44a0cebb29db8e75c7d7279117000c574
Dakshh1212 <thakraldaksh5040@gmail.com>
2025-05-16 12:55:51 +0530

    Display user email in each loan entry

--- models/User.js
+++ models/User.js
@@ -1,23 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+const transactionSchema = new mongoose.Schema({
+  text: String,
+  amount: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }
-});
+}, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Transaction", transactionSchema);
